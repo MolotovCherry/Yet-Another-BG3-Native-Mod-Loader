94c3b6f9e95a14aa52ce22d38fc11e7d48e86ff9
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2023-12-13 22:34:41 -0800

    Remove unneeded config options

--- src/config.rs
+++ src/config.rs
@@ -15,11 +15,6 @@ pub struct Core {
     /// The game's root installation directory,
     /// e.g. C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3
     pub install_root: PathBuf,
-    /// Extra command line flags to pass to the game upon startup
-    /// --skip-launcher flag is always passed
-    pub flags: Vec<String>,
-    /// Use steam to launch the game, recommended to leave this enabled
-    pub steam: bool,
     /// Which plugins to disable. Each entry is the plugins filename without extension
     /// Except for those in this list, all plugins are enabled by default
     /// e.g. FooBar.dll should have an entry for "FooBar"
@@ -31,8 +26,6 @@ impl Default for Core {
         Self {
             // the default location for most people
             install_root: r"C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3".into(),
-            flags: Vec::new(),
-            steam: true,
             disabled: Vec::new(),
         }
     }
