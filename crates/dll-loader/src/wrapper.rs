35f2635e5e07d5461ef20299cc50711ea1e623a9
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-02-24 15:05:33 -0800

    Try out dbghelp.dll

--- crates/dll-loader/src/wrapper.rs
+++ crates/dll-loader/src/wrapper.rs
@@ -1,20 +1,45 @@
-use std::ffi::c_void;
-
-use windows::{
-    core::{GUID, HRESULT},
-    Win32::Graphics::DirectWrite::DWRITE_FACTORY_TYPE,
+use windows::Win32::{
+    Foundation::{BOOL, HANDLE},
+    System::Diagnostics::Debug::{
+        MINIDUMP_CALLBACK_INFORMATION, MINIDUMP_EXCEPTION_INFORMATION, MINIDUMP_TYPE,
+        MINIDUMP_USER_STREAM_INFORMATION,
+    },
 };
-use windows_targets::link;
+use windows_dll::dll;
 
-// ordinarily I'd use windows-sys, but this function seems to be missing from it
-// while it's present in windows, it has too much magic to be useful
-link!("dwrite.dll" "system" fn DWriteCreateFactory(factorytype: DWRITE_FACTORY_TYPE, iid: *const GUID, factory: *mut *mut c_void) -> HRESULT);
+#[export_name = "MiniDumpWriteDump"]
+extern "system" fn mini_dump_write_dump(
+    hprocess: HANDLE,
+    processid: u32,
+    hfile: HANDLE,
+    dumptype: MINIDUMP_TYPE,
+    exceptionparam: *const MINIDUMP_EXCEPTION_INFORMATION,
+    userstreamparam: *const MINIDUMP_USER_STREAM_INFORMATION,
+    callbackparam: *const MINIDUMP_CALLBACK_INFORMATION,
+) -> BOOL {
+    #[dll(dwrite)]
+    extern "system" {
+        #[allow(non_snake_case)]
+        fn MiniDumpWriteDump(
+            hprocess: HANDLE,
+            processid: u32,
+            hfile: HANDLE,
+            dumptype: MINIDUMP_TYPE,
+            exceptionparam: *const MINIDUMP_EXCEPTION_INFORMATION,
+            userstreamparam: *const MINIDUMP_USER_STREAM_INFORMATION,
+            callbackparam: *const MINIDUMP_CALLBACK_INFORMATION,
+        ) -> BOOL;
+    }
 
-#[export_name = "DWriteCreateFactory"]
-extern "system" fn d_write_create_factory(
-    factorytype: DWRITE_FACTORY_TYPE,
-    iid: *const GUID,
-    factory: *mut *mut c_void,
-) -> HRESULT {
-    unsafe { DWriteCreateFactory(factorytype, iid, factory) }
+    unsafe {
+        MiniDumpWriteDump(
+            hprocess,
+            processid,
+            hfile,
+            dumptype,
+            exceptionparam,
+            userstreamparam,
+            callbackparam,
+        )
+    }
 }
