f225679181901c940c63a11f12fa0f171c91d739
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-10-07 12:32:55 -0700

    Fix broken single instance detection

--- crates/yet-another-bg3-mod-loader/src/single_instance.rs
+++ crates/yet-another-bg3-mod-loader/src/single_instance.rs
@@ -1,7 +1,9 @@
-use eyre::Result;
 use windows::{
     core::w,
-    Win32::{Foundation::ERROR_ALREADY_EXISTS, System::Threading::CreateMutexW},
+    Win32::{
+        Foundation::{GetLastError, ERROR_ALREADY_EXISTS},
+        System::Threading::CreateMutexW,
+    },
 };
 
 use crate::{helpers::OwnedHandle, popup::fatal_popup};
@@ -12,21 +14,35 @@ pub struct SingleInstance(OwnedHandle);
 impl SingleInstance {
     /// Panics and shows error popup if another instance of app already running
     /// If it succeeds, then the app will be considered free to open again once this instance drops
-    pub fn new() -> Result<Self> {
+    pub fn new() -> Self {
         let mutex = unsafe { CreateMutexW(None, true, w!("yet-another-bg3-mod-loader")) };
 
-        match mutex {
-            Ok(v) => Ok(Self(v.into())),
-
+        let handle: OwnedHandle = match mutex {
+            Ok(h) => h.into(),
             Err(e) => {
-                let message = if e.code() == ERROR_ALREADY_EXISTS.to_hresult() {
-                    "Another instance is already running"
-                } else {
-                    &format!("CreateMutexW failure: {e}")
-                };
-
-                fatal_popup("Yet Another Bg3 Mod Loader", message);
+                fatal_popup("Yet Another Bg3 Mod Loader", format!("mutex failed: {e}"));
             }
+        };
+
+        let err = unsafe { GetLastError() };
+        match err {
+            e if e == ERROR_ALREADY_EXISTS => {
+                fatal_popup(
+                    "Yet Another Bg3 Mod Loader",
+                    "Another instance is already running",
+                );
+            }
+
+            e if e.is_err() => {
+                fatal_popup(
+                    "Yet Another Bg3 Mod Loader",
+                    format!("CreateMutexW failure: {e:?}"),
+                );
+            }
+
+            _ => (),
         }
+
+        Self(handle)
     }
 }
