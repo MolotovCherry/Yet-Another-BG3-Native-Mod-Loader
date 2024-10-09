009f91baf974794289244d305d93ca3f64776efd
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-10-08 22:44:57 -0700

    Fix typo

--- crates/yet-another-bg3-mod-loader/src/tmp_loader.rs
+++ crates/yet-another-bg3-mod-loader/src/tmp_loader.rs
@@ -37,7 +37,7 @@ pub fn init_loader() -> Result<(usize, PathBuf)> {
     if check_hash && hash != LOADER_HASH {
         fatal_popup(
             "loader dll mismatch",
-            format!("loader.dll is either the wrong file, or corrupted. Please redownload the program to get a fresh copy of the 2/dll.\n\nExpected a hash of:\n{LOADER_HASH}\n\nbut instead found:\n{hash}"),
+            format!("loader.dll is either the wrong file, or corrupted. Please redownload the program to get a fresh copy of the exe/dll.\n\nExpected a hash of:\n{LOADER_HASH}\n\nbut instead found:\n{hash}"),
         );
     }
 
