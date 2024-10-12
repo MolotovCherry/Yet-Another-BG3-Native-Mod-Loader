d8eae95ad099b8ec9b19356bfba30952738666f1
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-10-12 13:21:20 -0700

    Fix partial copy compare + clear out lpcbneeded every iter

--- crates/yet-another-bg3-mod-loader/src/wapi/enum_modules.rs
+++ crates/yet-another-bg3-mod-loader/src/wapi/enum_modules.rs
@@ -1,13 +1,10 @@
 use eyre::Result;
 use tracing::{error, trace};
-use windows::{
-    core::HRESULT,
-    Win32::{
-        Foundation::{HMODULE, STILL_ACTIVE},
-        System::{
-            ProcessStatus::{EnumProcessModulesEx, LIST_MODULES_64BIT},
-            Threading::GetExitCodeProcess,
-        },
+use windows::Win32::{
+    Foundation::{ERROR_PARTIAL_COPY, HMODULE, STILL_ACTIVE},
+    System::{
+        ProcessStatus::{EnumProcessModulesEx, LIST_MODULES_64BIT},
+        Threading::GetExitCodeProcess,
     },
 };
 
@@ -46,10 +43,9 @@ fn is_alive(process: &OwnedHandle) -> bool {
 }
 
 fn inner_enum_modules(process: &OwnedHandle, modules: &mut Vec<HMODULE>) -> Result<()> {
-    // fully initialize this in order to set len
-    let mut lpcbneeded = 0;
-
     loop {
+        let mut lpcbneeded = 0;
+
         let size = (modules.len() * size_of::<HMODULE>()) as u32;
 
         let res = unsafe {
@@ -73,18 +69,11 @@ fn inner_enum_modules(process: &OwnedHandle, modules: &mut Vec<HMODULE>) -> Resu
         }
 
         if let Err(e) = res {
-            const ERROR_PARTIAL_COPY: HRESULT =
-                windows::Win32::Foundation::ERROR_PARTIAL_COPY.to_hresult();
-
             // This can be caused by:
             // - Process was terminated
             // - Missing permissions (try running as admin)
             // - Issues with disk / file(s) corrupt
-            const ERROR_PARTIAL_COPY_2: HRESULT = HRESULT::from_win32(0x8007012B);
-
-            trace!(?e, "EnumProcessModulesEx partial copy err");
-
-            if is_alive(process) && [ERROR_PARTIAL_COPY, ERROR_PARTIAL_COPY_2].contains(&e.code()) {
+            if is_alive(process) && ERROR_PARTIAL_COPY.to_hresult() == e.code() {
                 // retry again because it must've been a simple error
 
                 trace!(
@@ -110,13 +99,13 @@ fn inner_enum_modules(process: &OwnedHandle, modules: &mut Vec<HMODULE>) -> Resu
             "EnumProcessModulesEx passed"
         );
 
+        // To determine how many modules were enumerated by the call to EnumProcessModulesEx, divide the resulting
+        // value in the lpcbNeeded parameter by sizeof(HMODULE).
+        let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
+        modules.truncate(n_modules);
+
         break;
     }
-
-    // To determine how many modules were enumerated by the call to EnumProcessModulesEx, divide the resulting
-    // value in the lpcbNeeded parameter by sizeof(HMODULE).
-    let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
-    modules.truncate(n_modules);
 
     Ok(())
 }
