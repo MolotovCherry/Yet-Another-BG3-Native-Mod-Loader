2fada3cae80ad9768b5d9f81a98ffcf9f7c8dced
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-10-11 22:45:09 -0700

    Adjust enum_modules

--- crates/yet-another-bg3-mod-loader/src/loader/enum_modules.rs
+++ crates/yet-another-bg3-mod-loader/src/loader/enum_modules.rs
@@ -13,102 +13,15 @@ use windows::{
 
 use crate::helpers::OwnedHandle;
 
+/// IMPORTANT
+/// Do not call CloseHandle on any of the handles returned by this function. The information comes from a
+/// snapshot, so there are no resources to be freed.
 pub fn enum_modules(
     process: &OwnedHandle,
     mut cb: impl FnMut(HMODULE) -> Result<bool>,
 ) -> Result<()> {
-    fn inner(process: &OwnedHandle, modules: &mut Vec<HMODULE>) -> Result<()> {
-        // fully initialize this in order to set len
-        let mut lpcbneeded = 0;
-
-        loop {
-            let size = (modules.len() * size_of::<HMODULE>()) as u32;
-
-            let res = unsafe {
-                EnumProcessModulesEx(
-                    process.as_raw_handle(),
-                    modules.as_mut_ptr(),
-                    size as u32,
-                    &mut lpcbneeded,
-                    LIST_MODULES_64BIT,
-                )
-            };
-
-            if let Err(e) = res {
-                const ERROR_PARTIAL_COPY: HRESULT =
-                    windows::Win32::Foundation::ERROR_PARTIAL_COPY.to_hresult();
-
-                // This can be caused by:
-                // - Process was terminated
-                // - Missing permissions (try running as admin)
-                // - Issues with disk / file(s) corrupt
-                const ERROR_PARTIAL_COPY_2: HRESULT = HRESULT::from_win32(0x8007012B);
-
-                trace!(?e, "EnumProcessModulesEx partial copy err");
-
-                if is_alive(process)
-                    && [ERROR_PARTIAL_COPY, ERROR_PARTIAL_COPY_2].contains(&e.code())
-                {
-                    // retry again because it must've been a simple error
-
-                    trace!(
-                        lpcbneeded,
-                        size,
-                        len = modules.len(),
-                        "process is still alive"
-                    );
-
-                    // To determine if the lphModule array is too small to hold all module handles for the process,
-                    // compare the value returned in lpcbNeeded with the value specified in cb. If lpcbNeeded is greater
-                    // than cb, increase the size of the array and call EnumProcessModulesEx again.
-                    if lpcbneeded > size {
-                        let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
-                        trace!(new_len = n_modules, "resizing to len");
-                        modules.resize(n_modules, HMODULE::default());
-                    }
-
-                    continue;
-                }
-
-                error!(?e, "EnumProcessModulesEx received err");
-
-                return Err(e.into());
-            }
-
-            trace!(
-                lpcbneeded,
-                size,
-                len = modules.len(),
-                "EnumProcessModulesEx passed"
-            );
-
-            // To determine if the lphModule array is too small to hold all module handles for the process,
-            // compare the value returned in lpcbNeeded with the value specified in cb. If lpcbNeeded is greater
-            // than cb, increase the size of the array and call EnumProcessModulesEx again.
-            if lpcbneeded > size {
-                let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
-                trace!(new_len = n_modules, "resizing to len");
-                modules.resize(n_modules, HMODULE::default());
-                continue;
-            }
-
-            // IMPORTANT
-            // Do not call CloseHandle on any of the handles returned by this function. The information comes from a
-            // snapshot, so there are no resources to be freed.
-
-            break;
-        }
-
-        // To determine how many modules were enumerated by the call to EnumProcessModulesEx, divide the resulting
-        // value in the lpcbNeeded parameter by sizeof(HMODULE).
-        let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
-        modules.truncate(n_modules);
-
-        Ok(())
-    }
-
     let mut modules: Vec<HMODULE> = vec![HMODULE::default(); 1024];
-    inner(process, &mut modules)?;
+    inner_enum_modules(process, &mut modules)?;
 
     for module in modules {
         if !cb(module)? {
@@ -126,8 +39,93 @@ fn is_alive(process: &OwnedHandle) -> bool {
     match res {
         Ok(_) => code == STILL_ACTIVE.0 as u32,
         Err(e) => {
-            error!("GetExitCodeProcess: {e}");
+            error!(%e, "GetExitCodeProcess");
             false
         }
     }
+}
+
+fn inner_enum_modules(process: &OwnedHandle, modules: &mut Vec<HMODULE>) -> Result<()> {
+    // fully initialize this in order to set len
+    let mut lpcbneeded = 0;
+
+    loop {
+        let size = (modules.len() * size_of::<HMODULE>()) as u32;
+
+        let res = unsafe {
+            EnumProcessModulesEx(
+                process.as_raw_handle(),
+                modules.as_mut_ptr(),
+                size as u32,
+                &mut lpcbneeded,
+                LIST_MODULES_64BIT,
+            )
+        };
+
+        if let Err(e) = res {
+            const ERROR_PARTIAL_COPY: HRESULT =
+                windows::Win32::Foundation::ERROR_PARTIAL_COPY.to_hresult();
+
+            // This can be caused by:
+            // - Process was terminated
+            // - Missing permissions (try running as admin)
+            // - Issues with disk / file(s) corrupt
+            const ERROR_PARTIAL_COPY_2: HRESULT = HRESULT::from_win32(0x8007012B);
+
+            trace!(?e, "EnumProcessModulesEx partial copy err");
+
+            if is_alive(process) && [ERROR_PARTIAL_COPY, ERROR_PARTIAL_COPY_2].contains(&e.code()) {
+                // retry again because it must've been a simple error
+
+                trace!(
+                    lpcbneeded,
+                    size,
+                    len = modules.len(),
+                    %e,
+                    "error partial copy, but process is still alive, retry"
+                );
+
+                // To determine if the lphModule array is too small to hold all module handles for the process,
+                // compare the value returned in lpcbNeeded with the value specified in cb. If lpcbNeeded is greater
+                // than cb, increase the size of the array and call EnumProcessModulesEx again.
+                if lpcbneeded > size {
+                    let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
+                    trace!(new_len = n_modules, "resizing to len");
+                    modules.resize(n_modules, HMODULE::default());
+                }
+
+                continue;
+            }
+
+            error!(%e, "EnumProcessModulesEx received err");
+
+            return Err(e.into());
+        }
+
+        trace!(
+            lpcbneeded,
+            size,
+            len = modules.len(),
+            "EnumProcessModulesEx passed"
+        );
+
+        // To determine if the lphModule array is too small to hold all module handles for the process,
+        // compare the value returned in lpcbNeeded with the value specified in cb. If lpcbNeeded is greater
+        // than cb, increase the size of the array and call EnumProcessModulesEx again.
+        if lpcbneeded > size {
+            let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
+            trace!(new_len = n_modules, "resizing to len");
+            modules.resize(n_modules, HMODULE::default());
+            continue;
+        }
+
+        break;
+    }
+
+    // To determine how many modules were enumerated by the call to EnumProcessModulesEx, divide the resulting
+    // value in the lpcbNeeded parameter by sizeof(HMODULE).
+    let n_modules = lpcbneeded as usize / size_of::<HMODULE>();
+    modules.truncate(n_modules);
+
+    Ok(())
 }
