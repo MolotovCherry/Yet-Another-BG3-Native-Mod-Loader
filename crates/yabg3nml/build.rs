b06fc87d8c1dce97f5d31008b957f9427ea81919
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-10-20 22:43:37 -0700

    Convert frustrating build.rs code into justfile

--- crates/yabg3nml/build.rs
+++ crates/yabg3nml/build.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
-use std::{env, fs, time::UNIX_EPOCH};
-use std::{path::Path, time::SystemTime};
+use std::path::Path;
+use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
     if !cfg!(target_os = "windows") {
@@ -18,45 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     res.compile()?;
 
-    calc_hash()?;
-
-    env::set_var(
-        "REBUILD",
-        SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_nanos()
-            .to_string(),
-    );
-
-    println!("cargo:rerun-if-env-changed=REBUILD");
-
-    Ok(())
-}
-
-fn calc_hash() -> Result<(), Box<dyn Error>> {
-    // unfortunately RA needs this workaround to build locally
-
-    let env = env::var_os("OUT_DIR").unwrap();
-
-    let root = Path::new(&env)
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap();
-
-    let loader_dll = root.join("deps").join("loader.dll");
-
-    let data = fs::read(&loader_dll)?;
-
-    let hash = sha256::digest(&data);
-
-    println!("cargo::rustc-env=LOADER_HASH={hash}");
-
-    // we want this in the main folder tho
-    fs::copy(&loader_dll, root.join("loader.dll")).unwrap();
+    println!("cargo::rerun-if-env-changed=LOADER_HASH");
 
     Ok(())
 }
--- justfile
+++ justfile
@@ -1,13 +1,17 @@
-set shell := ["powershell.exe", "-c"]
+set windows-shell := ["powershell.exe", "-NoLogo", "-Command"]
 
-build:
-    cargo build -p yabg3nml
+target := `cargo target-dir`
 
-build-dev:
-    cargo build -p yabg3nml --features test-injection
+_build-loader flags:
+    cargo build -p loader {{flags}}
 
-build-ci:
-    cargo build -p yabg3nml --profile dev-ci
+_build-yabg3nml profile flags:
+    $env:LOADER_HASH = (Get-FileHash "{{target}}\{{profile}}\loader.dll" | Select-Object -ExpandProperty Hash).ToLower(); cargo build -p yabg3nml {{flags}}
 
-build-release:
-    cargo build -p yabg3nml --release
+build: (_build-loader "") (_build-yabg3nml "debug" "")
+
+build-dev: (_build-loader "") (_build-yabg3nml "debug" "--features test-injection")
+
+build-ci: (_build-loader "--profile dev-ci") (_build-yabg3nml "dev-ci" "--profile dev-ci")
+
+build-release: (_build-loader "--release") (_build-yabg3nml "release" "--release")
