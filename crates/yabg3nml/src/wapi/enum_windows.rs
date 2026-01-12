ca2f20776dafd270254a31feb4ffdcb5f625cbb8
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-01-12 15:54:58 -0800

    Simplify EnumWindows cb

--- crates/yabg3nml/src/wapi/enum_windows.rs
+++ crates/yabg3nml/src/wapi/enum_windows.rs
@@ -1,10 +1,10 @@
 use std::{
     mem,
     panic::{self, AssertUnwindSafe},
+    ptr,
 };
 
 use eyre::Result;
-use sayuri::sync::Mutex;
 use tracing::{error, trace_span};
 use windows::{
     Win32::{
@@ -16,75 +16,46 @@ use windows::{
 
 type UserCallback<'a> = Box<dyn FnMut(HWND) -> Result<()> + Send + Sync + 'a>;
 
-struct FfiCb(UserCallback<'static>);
-
-trait StaticFfiCbMethods {
-    unsafe fn set_cb(&self, cb: UserCallback);
-    unsafe fn call(&self, hwnd: HWND) -> Result<()>;
-    fn drop(&self);
-}
-
-impl StaticFfiCbMethods for Mutex<Option<FfiCb>> {
-    /// SAFETY:
-    /// Caller promises to drop before end of scope where original closure was created
-    ///     this is because closure may have captures, this is !'static
-    unsafe fn set_cb(&self, cb: UserCallback) {
-        let _static = unsafe { mem::transmute::<UserCallback, UserCallback<'static>>(cb) };
-        *self.lock() = Some(FfiCb(_static));
-    }
-
-    /// SAFETY:
-    /// This must be called in scope where closure captures are still valid
-    unsafe fn call(&self, hwnd: HWND) -> Result<()> {
-        if let Some(cb) = &mut *self.lock() {
-            cb.0(hwnd)
-        } else {
-            Ok(())
-        }
-    }
-
-    fn drop(&self) {
-        *self.lock() = None;
-    }
-}
-
-static CB: Mutex<Option<FfiCb>> = Mutex::new(None);
-
 #[allow(non_snake_case)]
 pub fn EnumWindowsRs(cb: impl FnMut(HWND) -> Result<()> + Send + Sync) {
     let span = trace_span!("EnumWindowsRs");
     let _guard = span.enter();
 
-    unsafe {
-        CB.set_cb(Box::new(cb));
-    }
-
-    _ = unsafe { EnumWindows(Some(enum_cb), LPARAM(0)) };
-
-    CB.drop();
+    let mut cb: UserCallback = Box::new(cb);
+    _ = unsafe {
+        EnumWindows(
+            Some(enum_cb),
+            LPARAM((&raw mut cb).expose_provenance() as _),
+        )
+    };
 }
 
-extern "system" fn enum_cb(param0: HWND, _: LPARAM) -> BOOL {
+extern "system" fn enum_cb(param0: HWND, cb: LPARAM) -> BOOL {
     let span = trace_span!("enum_cb");
     let _guard = span.enter();
 
-    let result = panic::catch_unwind(AssertUnwindSafe(|| unsafe { CB.call(param0) }));
+    let cb_raw: *mut UserCallback = ptr::with_exposed_provenance_mut(cb.0 as _);
+    let cb = unsafe { &mut *cb_raw };
 
-    match result {
+    let result = panic::catch_unwind(AssertUnwindSafe(|| cb(param0)));
+
+    let res = match result {
         // no panic and cb returned Ok
-        Ok(Ok(_)) => true.into(),
+        Ok(Ok(_)) => true,
 
         // no panic and callback returned Err
         Ok(Err(err)) => {
             error!("{err}");
-            false.into()
+            false
         }
 
         // panic
         Err(e) => {
             // so it never panics
             mem::forget(e);
-            false.into()
+            false
         }
-    }
+    };
+
+    res.into()
 }
